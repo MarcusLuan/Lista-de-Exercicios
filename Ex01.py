7d09645c71c1d5b9ba2e0dbeb701ca713a70c2d5
MarcusLuan <125037376+MarcusLuan@users.noreply.github.com>
2025-03-26 22:51:38 -0300

    Ex01.py o que é
    1. Peça ao usuário três notas e calcule:
    a. A média aritmética simples
    b. A média ponderada considerando os pesos 2, 2 e 3
    c. A média ponderada considerando os pesos 1, 2 e 2

---
+++ Ex01.py
@@ -0,0 +1,7 @@
+Nota1 = int(input("Digite a 1º nota do aluno: "))
+Nota2 = int(input("Digite a 2º nota do aluno: "))
+Nota3 = int(input("Digite a 3º nota do aluno: "))
+
+print("A média aritimética simples do aluno é: ", (Nota1+Nota2+Nota3)/3)
+print("A média aritimética ponderada do aluno considerando os pesos 2, 2 e 3 é: ", (Nota1*2+Nota2*2+Nota3*3)/7)
+print("A média aritimética ponderada do aluno considerando os pesos 1, 2 e 2 é: ", (Nota1*1+Nota2*2+Nota3*2)/5)
