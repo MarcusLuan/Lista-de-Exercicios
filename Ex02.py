c128cadb60879929d9c9cd5c08e5a5c428d816b8
MarcusLuan <125037376+MarcusLuan@users.noreply.github.com>
2025-03-27 14:59:21 -0300

    Ex02.py
    2. Solicite um valor em segundos e converta para dias, horas e minutos.

---
+++ Ex02.py
@@ -0,0 +1,7 @@
+segundos = int(input("Digite os segundos para converter: "))
+minutos = segundos // 60
+horas = segundos // 3600
+dias = segundos // 86400
+print(f"Segundos para minutos: {minutos}")
+print(f"Segundos para horas: {horas}")
+print(f"Segundos para dias: {dias}")
